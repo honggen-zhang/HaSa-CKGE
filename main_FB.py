57f5959a16710f786b84d98f731276e4527fb38d
honggen-zhang <56853824+honggen-zhang@users.noreply.github.com>
2023-10-14 15:48:38 -1000

    Update and rename main.py to main_FB.py

--- main_FB.py
+++ main_FB.py
@@ -2,7 +2,6 @@ import torch
 import torch.backends.cudnn as cudnn
 #import logging
 from train import train
-from train_bert import train_bert
 ngpus_per_node = torch.cuda.device_count()
 print('# gpus:',ngpus_per_node)
 
@@ -17,15 +16,14 @@ Hasa_model = 'HaSa'
 #Hasa_model = 'HaSa_Hard_Bias'
 #Hasa_model = 'HaSa_wohard_Bias'
 pretrain_model = 'sentence-transformers/all-mpnet-base-v2'
-#pretrain_model = 'bert-base-uncased'
 input_dim = 768
 em_dim = 500
 epoch_step = 5
 tau = 1e-4
 data_name = 'FB15K237_L/' 
-Resuming = True
-loading_model = 'dynamicgraph_lts/Honggen/HaSa/modelWWW/FB_plus/bestmodel_1e4_8.pth.tar'
-#loading_model = None
+Resuming = False
+#loading_model = '/FB_plus/bestmodel_1e4_8.pth.tar'
+loading_model = None
 train(batch_size = batch_size,
            learn_rate = learn_rate,
       num_false_neg = num_false_neg,
@@ -42,5 +40,5 @@ train(batch_size = batch_size,
       Resuming=Resuming,
       loading_model=loading_model,
       schedule_with_warmup=False,
-      plus = True)
+      plus = False)
 
